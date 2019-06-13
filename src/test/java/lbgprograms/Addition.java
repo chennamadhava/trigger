b74ca6697d707b298fb1bcb20f93a6a734cf658a
nagarjunarc <nagarjunarc@HDDLBGKIOSK10.Virtusa.com>
2019-06-13 16:48:03 +0530

    build trigger

---
+++ src/test/java/lbgprograms/Addition.java
@@ -0,0 +1,20 @@
+package lbgprograms;
+
+public class Addition {
+
+	public int additionOfTwoNumbers() {
+
+		int a,b,c;
+		a=10;
+		b=20;
+		c=a+b;
+
+		return c;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Addition ad=new Addition();
+		ad.additionOfTwoNumbers();
+	}
+
+}
